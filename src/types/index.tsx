4d2c40cbcf1b628559a6ab5adc90db1e98ab3d08
kaller <rexrayo15@gmail.com>
2022-05-03 15:51:54 -0400

    Types files

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,6 +1,8 @@
 export type RootStackParamList = {
     Login: undefined;
     Root: undefined;
+    CreateProfile: undefined;
+    ChatRoom: { id: string, user: string };
   };
 
 export type TabsParamList = {
@@ -8,3 +10,44 @@ export type TabsParamList = {
   Dashboard: undefined;
   Profile: undefined;
 };
+
+export type ChatParamItem = {
+  item:{
+    id: string;
+    user: string;
+    message?: string;
+    time?: string;
+    avatar?: string;
+  },
+}
+
+export type User = {
+  id: string;
+  user: string;
+}
+
+export type SignData = {
+  email: string;
+  password: string;
+}
+
+export type Account = {
+  user_id: string;
+  user_name: string;
+  user_description: string;
+  user_image: string;
+  user_theme_manual: boolean;
+  user_theme_automatic: boolean;
+}
+
+export type Message = {
+  id: string;
+  content: string;
+  createdAt: string;
+  user: User;
+}
+
+export type ChatMessageProps = {
+  message: Message;
+  myId: string,
+}
